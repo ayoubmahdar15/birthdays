ba1b038213722ff3b9c7abc6f3ec19866ebc157d
ayoubmahdar15 <ayoubmahdar15@users.noreply.github.com>
2021-01-10 23:19:54 +0000

    automated commit by submit50

--- mario.c
+++ mario.c
@@ -11,17 +11,20 @@ int main(void)
         {
              height = get_int("Enter height: ");
         }
-       while (height < 1 || height > 8);
+        while (height < 1 || height > 8);
 
     // Rows 
     for (int i = 0; i < height ; i++)   
     {
         // Columns producing "# or  "
         for (int m = 0; m < (height - (i + 1)) ; m++)
+            
             printf(" ");
      
         for (int c = 0; c < i + 1; c++)
+            
             printf("#");
+           
            
        printf("\n");
     }
