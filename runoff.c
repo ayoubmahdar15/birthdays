79ca2745e03a3030d2ca8b6ba210012470b396e1
ayoubmahdar15 <ayoubmahdar15@users.noreply.github.com>
2021-01-19 20:46:02 +0000

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -195,7 +195,7 @@ bool is_tie(int min)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].eliminated == true && candidates[i].votes == min)
+        if (candidates[i].eliminated == false && candidates[i].votes != min)
         {
             return true;
         }
