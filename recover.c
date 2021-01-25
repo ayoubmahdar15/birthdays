ab6afcc1eb92586e1ec2d5f2e4cec45e77aeae8b
ayoubmahdar15 <ayoubmahdar15@users.noreply.github.com>
2021-01-25 22:28:57 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -6,46 +6,42 @@ int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
-        printf("Usage: ./recover image\n");
-        return 1;
+        printf("Incorrect usage\n");
+        return 1; 
     }
     
-    // open memory card
-    FILE *input_card = fopen(argv[1], "r"); 
-    // Memory card cannot be read 
-    if (input_card == NULL)
+    FILE *memory_card = fopen(argv[1], "r"); 
+    if (memory_card == NULL)
     {
-        printf("Cannot be read\n"); 
-        return 1;
+        printf("Cannot be opened for reading\n");
+        return 1; 
     }
     
-    unsigned char buffer[512]; // Buffer for Array
-    int count_images = 0; // Counting # of Images Recovered
-    char filename[8];
-    FILE *output_images = NULL;// File Pointer for Output
+    unsigned char buffer [512];
+    int image_count = 0; 
+    FILE *output_images = NULL; 
+    char filename [8];
     
-    // Read Memory Card
-    while (fread(buffer, 512, 1, input_card) != 0)
+    while (fread(&buffer, 512, 1, memory_card))
     {
-        // Start of New JPEG
-        if (buffer[0] == 0xff && buffer [1] == 0xd8 && buffer [2] == 0xff && (buffer [3] & 0xf0) == 0xe0) 
+        if ((buffer[0] == 0xff && buffer [1] == 0xd8 && buffer [2] == 0xff && (buffer [3] & 0xf0) == 0xe0))
         {
-            // write jpeg into file name (000.jpg, 001.jpg...)  
-            sprintf(filename, "%03i.jpg", count_images);
-            // open filename   
+            // write jpeg into file name (000.jpg, 001.jpg...)
+            sprintf(filename, "%03i.jpg", image_count);
+            // open filename
             output_images = fopen(filename, "w");
-            // keep track of images found 
-            count_images++; 
+            // keep track of images found
+            image_count++;
         }
-        // Writing into New File 
-        if (output_images != NULL)
+        
+        if (output_images != 0)
         {
-            fwrite(&buffer, 512, 1, output_images);                            
+            fwrite(&buffer, 512, 1, output_images);
         }
     }
-    // Close Files 
-    fclose(input_card); 
-    fclose(output_images); 
     
-    return 0; 
+    fclose(output_images); 
+    fclose(memory_card); 
+    
+    return 0;
 }
