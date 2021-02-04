06485911e8f4f870c3b85229bc63bce40dd09e5a
ayoubmahdar15 <ayoubmahdar15@users.noreply.github.com>
2021-02-04 19:33:06 +0000

    automated commit by check50 [check50=True]

--- mario.py
+++ mario.py
@@ -1,12 +1,14 @@
 from cs50 import get_int
 
 while True:
-    ## User input for Height 
+    # User input for Height 
     height = get_int("Height: ") 
     if height < 1 or height > 8:
         height = get_int("Height: ")
+    if height >= 1 and height <= 8:
+        break
     
-    ## Pattern for printing 
+    # Pattern for printing 
 for i in range(height):
     print ((height - 1 - i) * " ", end="")
     print ((i + 1) * "#")
